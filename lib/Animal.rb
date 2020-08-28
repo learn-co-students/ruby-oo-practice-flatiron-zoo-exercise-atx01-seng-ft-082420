b9187ccf61a73b5e7e88cdf1c5baa1bd430f8fba
devindavis5 <devinrdavis555@gmail.com>
2020-08-28 16:10:56 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,22 @@
 class Animal
+    attr_reader :species, :nickname
+    attr_accessor :weight, :zoo
+    @@all = []
+    def initialize(species, nickname, weight, zoo)
+        @species = species
+        @nickname = nickname
+        @weight = weight
+        @zoo = zoo
+        self.class.all << self
+    end
 
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(animal_species)
+        self.all.select do |animal|
+            animal.species == animal_species
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -3,7 +3,17 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+texas_zoo = Zoo.new('Texas Zoo', 'Texas')
+florida_zoo = Zoo.new('Florida Zoo', 'Florida')
+cali_zoo = Zoo.new('California Zoo', 'California')
+second_florida_zoo = Zoo.new('Second Florida Zoo', 'Florida')
+
+hermione = Animal.new('Lion', 'Hermione', 250, texas_zoo)
+hagrid = Animal.new('Octopus', 'Hagrid', 250, florida_zoo)
+harry = Animal.new('Tiger', 'Harry', 220, florida_zoo)
+ron = Animal.new('Bear', 'Ron', 350, cali_zoo)
+ron2 = Animal.new('Bear', 'Ron2', 360, cali_zoo)
+
 
 
 binding.pry
