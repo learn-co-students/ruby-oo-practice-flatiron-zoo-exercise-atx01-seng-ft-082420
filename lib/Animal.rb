aaa26d0b2dd8a5a41e63bd8fbc238f22d9771dac
siegelverse <msiegel37@gmail.com>
2020-09-01 09:10:06 -0500

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Animal
+    attr_reader :nickname, :zoo
+    attr_accessor :weight, :species
+    @@all = []
+    def initialize(species, weight, nickname)
+        @species = species 
+        @weight = weight 
+        @nickname = nickname 
+        self.class.all << self
+    end 
 
+    def self.find_by_species(species)
+        Animal.all.select do |animal|
+            animal.species == species
+        end 
+    end
+    
+    def self.all 
+        @@all 
+    end 
+    #binding.pry
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,44 @@
+require 'pry'
 class Zoo
+    attr_reader :name, :location
+    @@all = []
+    def initialize(name, location)
+        @name = name
+        @location = location 
+        self.class.all << self
+    end
 
+    def animals
+        Animal.all.select do |a|
+            a.zoo == self 
+        end 
+    end 
+
+    def animal_species
+        self.animals.collect do |a|
+            a.species.uniq 
+        end 
+    end
+    
+    def find_by_species(species)
+        self.animals.select do |a|
+            a.species == species
+        end 
+    end
+
+    def animal_nicknames
+        self.animals.collect do |a|
+            a.nickname 
+        end 
+    end 
+
+    def self.find_by_location(location)
+        self.all.select do |z| 
+            z.location == location 
+        end 
+    end 
+
+    def self.all
+        @@all
+    end 
 end
